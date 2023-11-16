0c9f15c30b5beeb8616a7a58eaf36e503d6038eb
Khoa học dữ liệu 17A3 <lhtrang.dhkl17a3hn@sv.uneti.edu.vn>
2023-11-16 21:47:45 +0700

    them nhieu file

---
+++ chuong 8/bai_tap_8.11.py
@@ -0,0 +1,4 @@
+n=int(input("Nhập giá trị n:"))
+x=int(input("Nhập giá trị x:"))
+A=(x*x + x + 1)^n + (x*x - x +1)^n 
+print("Giá trị của A:", A)
---
+++ chuong 8/bai_tap_8.14.py
@@ -0,0 +1,5 @@
+x=int(input("Nhập số nguyên thứ 1:"))
+y=int(input("Nhập số nguyên thứ 2:"))
+z=int(input("Nhập số nguyên thứ 3:"))
+N=x+y+z
+print("Tính tổng của N:",N)
